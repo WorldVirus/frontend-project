c4510a348be07a671f85ae665d096128cf02b59d
WorldVirus <metallika956@gmail.com>
2017-09-17 14:00:59 +0300

    firstDz

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const  http = require('http');
+const notFound = require('./routes/notFound');
+const index = require('./routes/index');
+const publication = require('./routes/public');
+
+http.createServer((req,res)=>{
+    if (req.url.match(/\.(html|css|png|js|jpg)$/)) {
+    publication(req,res);
+    }
+        else if ((req.url.startsWith('/index') || (req.url === '/'))) {
+
+            index(req,res);
+        }
+
+    else {
+        notFound(req,res);
+    }
+
+
+}).listen(8000,() => console.log("Hello fucking world !"));
