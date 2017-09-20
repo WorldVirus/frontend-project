48a276e56b838c0d1c5828a697263001d3980108
WorldVirus <metallika956@gmail.com>
2017-09-20 18:34:15 +0300

    fixed view and server

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 'use strict';
 
-const  http = require('http');
-const notFound = require('./routes/notFound');
-const index = require('./routes/index');
-const publication = require('./routes/public');
+const express = require('express');
 
-http.createServer((req,res)=>{
-    if (req.url.match(/\.(html|css|png|js|jpg)$/)) {
-    publication(req,res);
-    }
-        else if ((req.url.startsWith('/index') || (req.url === '/'))) {
+const app = express();
 
-            index(req,res);
-        }
+app.use(express.static('public'));
 
-    else {
-        notFound(req,res);
-    }
+app.get('*', (req, res) => {
+   // res.send('404');
+    res.redirect('/error.html')
 
+});
 
-}).listen(process.env.PORT || '8080',() => console.log("Hello fucking world !"));
+app.listen(process.env.PORT || '8080', () => {
+    console.log('Hello fucking world !');
+});
