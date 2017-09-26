41a55c0480bb5888b6f795121d0c01c0167267a8
WorldVirus <metallika956@gmail.com>
2017-09-26 09:37:19 +0300

    registration module

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,82 @@
 
-const fs = require('fs');
-const path =require('path');
+ Just for testing and practic with DOM and SPA
 
-function index(req,res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
+'use strict'
+document.getElementById('textBlock').style.visibility = 'hidden';
+document.getElementById('login').style.visibility = 'hidden';
+document.getElementById('buttonBack').style.visibility = 'hidden';
+document.getElementById('registration').style.visibility = 'hidden';
 
-    const stream = fs.createReadStream(path.resolve('public','index.html'));
+document.getElementById('buttonSecond').onclick = function() {
+    document.getElementById('buttonBack').style.visibility = 'visible';
+    document.getElementById('main').style.visibility = 'hidden';
+    document.getElementById('textBlock').style.visibility = 'visible';
+    document.getElementById('buttonBack').onclick = function() {
+        document.getElementById('textBlock').style.visibility = 'hidden';
+        document.getElementById('main').style.visibility = 'visible';
+    };
+};
 
-    stream.pipe(res);
+document.getElementById('buttonFirst').onclick = function() {
+    document.getElementById('buttonBack').style.visibility = 'visible';
+    document.getElementById('main').style.visibility = 'hidden';
+    document.getElementById('login').style.visibility = 'visible';document.getElementById('buttonBack').onclick = function() {
+        document.getElementById('login').style.visibility = 'hidden';
+        document.getElementById('main').style.visibility = 'visible';
+    };
+};
+
+document.getElementById('rg').onclick = function() {
+    document.getElementById('buttonBack').style.visibility = 'visible';
+    document.getElementById('login').style.visibility = 'hidden';
+    document.getElementById('registration').style.visibility = 'visible';
+    document.getElementById('buttonBack').onclick = function() {
+        document.getElementById('registration').style.visibility = 'hidden';
+        document.getElementById('login').style.visibility = 'visible';
+    };
+
+};
+
+
+
+//Validation
+
+function showError(container, errorMessage) {
+    container.className = 'error';
+    var msgElem = document.createElement('span');
+    msgElem.className = "error-message";
+    msgElem.innerHTML = errorMessage;
+    container.appendChild(msgElem);
 }
-module.exports = index;
+
+function resetError(container) {
+    container.className = '';
+    if (container.lastChild.className === "error-message") {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function validate(form) {
+    var elems = form.elements;
+    console.log(elems.username.value.length);
+    resetError(elems.username.parentNode);
+    if (!elems.username.value ) {
+        showError(elems.username.parentNode, ' What is your username ?');
+        return ;
+    }
+    else if (!isNaN(elems.username.value)) {
+        showError(elems.username.parentNode, ' Username have to be a type text');
+        return;
+    }
+    resetError(elems.password.parentNode);
+    if (!elems.password.value) {
+        showError(elems.password.parentNode, ' What is your password  ?');
+    } else if (elems.password.value !== elems.password2.value) {
+        showError(elems.password.parentNode, ' Passwords is not the same');
+    }
+    else if (elems.password.value.length <4) {
+        showError(elems.password.parentNode, ' Passwords is too short');
+    }
+
+
+}
