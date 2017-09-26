c902fe8a1fc24cd34f55c5a897c78a3c744099ce
WorldVirus <metallika956@gmail.com>
2017-09-26 09:48:21 +0300

    block module

---
+++ public/information/info.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    const Block = window.Block;
+    const Info = window.Info;
+
+    class Info extends Block {
+        constructor() {
+            const el = document.createElement('div');
+            super(el);
+        }
+
+
+    }
+
+    window.Info = Info;
+
+})();
